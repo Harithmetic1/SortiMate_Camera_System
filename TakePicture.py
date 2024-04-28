7b71acbba612a082c10bf0e893c72f9845596248
Harith Onigemo <onigemotosin@gmail.com>
2024-04-29 02:41:00 +0400

    camera adjust

--- TakePicture.py
+++ TakePicture.py
@@ -17,15 +17,30 @@ class CameraHandler:
         cv2.destroyAllWindows()
         self.cam = cv2.VideoCapture(0)
 
-    def streamVideo(self):
+    def streamVideo(self, zoom_factor=0.5):  # Add zoom_factor argument
         while True:
             ret, image = self.cam.read()
-            cv2.imshow('Imagetest',image)
+
+            # Get image width and height
+            width = image.shape[1]
+            height = image.shape[0]
+
+            # Define new width and height based on zoom factor
+            new_width = int(width * zoom_factor)
+            new_height = int(height * zoom_factor)
+
+            # Calculate starting pixel coordinates for cropping
+            start_x = int((width - new_width) / 2)
+            start_y = int((height - new_height) / 2)
+
+            # Crop the image
+            cropped_image = image[start_y:start_y + new_height, start_x:start_x + new_width]
+
+            cv2.imshow('Imagetest', cropped_image)
             k = cv2.waitKey(1)
             if k != -1:
                 break
-        # self.cam.release()
-        cv2.destroyAllWindows()
+            cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     cam = CameraHandler()
